b354914524845eff42df408d9637c463dae36042
splitmoong <theboibrainiac@gmail.com>
2023-10-11 09:18:55 +0530

    first commit

---
+++ sqlthings.sql
@@ -0,0 +1,10 @@
+CREATE TABLE players(
+    player_username varchar(50),
+    player_password varchar(50),
+    score_topic01 int,
+    score_topic02 int,
+    score_topic03 int,
+    score_topic04 int
+);
+select * from players
+
